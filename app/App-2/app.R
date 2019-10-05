7b67fc50cf15c3e4c459ba9ad2a7cb284075f23d
abrarShariar <shariarabrar@gmail.com>
2019-10-05 14:38:30 +0600

    scrrencasts

---
+++ app/App-2/app.R
@@ -0,0 +1,44 @@
+library(maps)
+library(mapproj)
+source("helpers.R")
+counties <- readRDS("data/counties.rds")
+
+# User interface ----
+ui <- fluidPage(
+  titlePanel("censusVis"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      helpText("Create demographic maps with 
+        information from the 2010 US Census."),
+      
+      selectInput("var", 
+                  label = "Choose a variable to display",
+                  choices = c("Percent White", "Percent Black",
+                              "Percent Hispanic", "Percent Asian"),
+                  selected = "Percent White"),
+      
+      sliderInput("range", 
+                  label = "Range of interest:",
+                  min = 0, max = 100, value = c(0, 100))
+    ),
+    
+    mainPanel(plotOutput("map"))
+  )
+)
+
+# Server logic ----
+server <- function(input, output) {
+  output$map <- renderPlot({
+    data <- switch(input$var, 
+                   "High risk" = counties$white,
+                   "Moderate risk" = counties$black,
+                   "Mild risk" = counties$hispanic,
+                   "Low risk" = counties$asian)
+    
+    percent_map(var = data, color = "red", legend.title = "Risk distribution", max = 100, min = 0)
+  })
+}
+
+# Run app ----
+shinyApp(ui, server)
